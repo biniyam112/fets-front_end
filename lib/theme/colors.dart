1e0cb87df5bd760cfd55c3bdf877a712c44461e9
biniyam112 <biniyamdemissew112@gmail.com>
2022-05-01 14:27:25 +0300

    theme

---
+++ lib/theme/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const backgroundColor = Color(0xFFF7F7F7);
+const primaryColor = Color(0xff266EF1);
+const lightTextColor = Color(0xff8C91A0);
+const secondaryColor = Color(0xff129113);
+const errorColor = Color(0xffDA2B2B);
+const filterBackgroundColor = Color(0xff6F6262);
+const lightBlueBackround = Color(0xffF6F9FE);
