a2eb7e76e12b4c5c360c3e606bee60eab80fdaf3
yididiya1 <yididiyakebede@gmail.com>
2022-05-29 15:24:23 +0300

    hive integration

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,11 @@ import 'package:fets_mobile/services/services.dart';
 import 'package:fets_mobile/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
-main() {
+main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
   runApp(const MyApp());
 }
 
